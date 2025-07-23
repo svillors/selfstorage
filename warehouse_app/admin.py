654f214704c2bd3cedc6de635c827691c9db922f
MaxSmile44 <afmaxim89@gmail.com>
2025-07-23 23:22:22 +0300

    Переместил и изменил ф-цию save_model в admin.py из WarehouseAdmin в OrderAdmin

--- warehouse_app/admin.py
+++ warehouse_app/admin.py
@@ -19,13 +19,6 @@ class WarehouseAdmin(admin.ModelAdmin):
     list_display = ['city', 'address', 'free_boxes_count_display']
     inlines = [BoxesInline]
 
-    def save_model(self, request, obj, form, change):
-        super().save_model(request, obj, form, change)
-        for box in obj.boxes.all():
-            if not box.orders.exists():
-                box.is_busy = False
-                box.save(update_fields=['is_busy'])
-
     def free_boxes_count_display(self, obj):
         return obj.free_boxes_count
     free_boxes_count_display.short_description = "Свободных боксов"
@@ -35,6 +28,13 @@ class WarehouseAdmin(admin.ModelAdmin):
 class OrderAdmin(admin.ModelAdmin):
     inlines = [StuffInline]
     list_display = ['box', 'date_start', 'date_end']
+
+    def save_model(self, request, obj, form, change):
+        super().save_model(request, obj, form, change)
+        for order in Order.objects.select_related('box').all():
+            if order.box.orders.exists():
+                order.box.is_busy = True
+                order.box.save(update_fields=['is_busy'])
 
 
 @admin.register(Box)
