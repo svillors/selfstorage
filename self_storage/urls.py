76fde0836cdb6745692cd281deb6712b76eb09f4
Raman <romansashin04@gmail.com>
2025-07-23 14:10:54 +0300

    Подключение статики и кнопок для перехода на другой шаблон

--- self_storage/urls.py
+++ self_storage/urls.py
@@ -16,7 +16,17 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from warehouse_app import views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', views.index, name='index'),
+    path('faq/', views.faq, name='faq'),
+    path('boxes/', views.boxes, name='boxes'),
 ]
+
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- warehouse_app/views.py
+++ warehouse_app/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+
+def faq(request):
+    return render(request, 'faq.html')
+
+
+def boxes(request):
+    return render(request, 'boxes.html')
