b69f144c70ce07afcee94e39a2556260bedc07fd
Raman <romansashin04@gmail.com>
2025-07-23 15:26:26 +0300

    Добавлена регистрация и входа

---
+++ warehouse_app/forms.py
@@ -0,0 +1,15 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
+from django.contrib.auth.models import User
+
+
+class RegisterForm(UserCreationForm):
+    email = forms.EmailField(required=True)
+
+    class Meta:
+        model = User
+        fields = ['email', 'username', 'password1', 'password2']
+
+
+class LoginForm(AuthenticationForm):
+    username = forms.CharField(label='Email / Username')
